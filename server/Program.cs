f7546412730ba9a6b396bbdbf76f6bc725432574
siru <sirius65535@gmail.com>
2022-03-26 22:58:50 +0800

    make code cleaner

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,105 +8,81 @@ class Server
 {
     public static void Main()
     {
-        TcpListener server = null;
-        List<Player> players = new List<Player>();
+        Int32 port = 12345;
         int counter = 0;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        TcpListener server = new TcpListener(localAddr, port);
+        server.Start();
+        Console.WriteLine("Server start!");
+
         try
         {
-            Int32 port = 12345;
-            // IPAddress localAddr = IPAddress.Parse(GetLocalIPAddress());
-            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-            server = new TcpListener(localAddr, port);
-            server.Start();
-
-            Byte[] bytes = new Byte[256];
-
             while (true)
             {
-                TcpClient client = server.AcceptTcpClient();
                 counter += 1;
+                TcpClient client = server.AcceptTcpClient();
 
-                Player player = new Player(client, Convert.ToString(counter));
-                players.Add(player);
+                StartTheThread(client, Convert.ToString(counter));
+                Console.WriteLine(Process.GetCurrentProcess().Threads.Count);
             }
         }
-        catch (SocketException e)
+        catch (Exception ex)
         {
-            Console.WriteLine("SocketException: {0}", e);
+            Console.WriteLine(ex.ToString());
         }
         finally
         {
-            if (server != null)
-            {
-                server.Stop();
-            }
+            server.Stop();
         }
 
-        Console.WriteLine("\nHit enter to continue...");
-        Console.Read();
     }
-    private static string GetLocalIPAddress()
+
+    static public void StartTheThread(TcpClient param1, string param2)
     {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
+        var t = new Thread(() => KeepListening(param1, param2));
+        t.Start();
+    }
+
+    private static void KeepListening(TcpClient client, string name)
+    {
+        string clientIp = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+        string info = $"IP: {clientIp}, name: {name}";
+        Console.WriteLine($"Client connected with {info}");
+
+        try
         {
-            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            while (client.Connected)
             {
-                return ip.ToString();
-            }
-        }
-        throw new Exception("No network adapters with an IPv4 address in the system!");
-    }
-}
-public class Player
-{
-    TcpClient clientSocket;
-    string clNo = String.Empty;
-    public Player(TcpClient inClientSocket, string clineNo)
-    {
-        this.clientSocket = inClientSocket;
-        this.clNo = clineNo;
-        Thread ctThread = new Thread(keepListening);
-        ctThread.Start();
-    }
-    private void keepListening()
-    {
-        byte[] bytes = new byte[128];
-
-        Console.WriteLine("Socket: " + clNo + " connected!");
-        while (clientSocket.Connected)
-        {
-            try
-            {
-                NetworkStream stream = clientSocket.GetStream();
-                Console.WriteLine("if this message shows too many times, it is a bug.");
-
-                int i;
-
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    String data = Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
 
-                    data = data.ToUpper();
+                    string msg = Encoding.ASCII.GetString(buffer);
+                    Console.WriteLine($"Receive {msg} from client: {info}");
 
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    msg = msg.ToUpper();
 
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("Sent: {0}", data);
+                    buffer = Encoding.ASCII.GetBytes(msg);
+
+                    stream.Write(buffer, 0, bytesRead);
+                    Console.WriteLine($"Send {msg} to client: {info}");
                 }
 
                 stream.Flush();
-                // must Close, idk why
                 stream.Close();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-                break;
-            }
+
         }
-        Console.WriteLine("Socket: " + clNo + " disconnected!");
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            client.Close();
+            Console.WriteLine($"Client disconnected with {info}");
+        }
     }
 }
